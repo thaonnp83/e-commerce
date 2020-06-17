7e31821db7738fab200cd60bb8ee0def98f2e4e8
thaonnp83 <jupiter.planet@gmail.com>
2020-06-17 13:14:29 +0700

    * Brand.cs: * Category.cs: * ProductController.cs: * CategoryController.cs: * WeatherForecastController.cs:
    * Product.cs: Add controllers

---
+++ ECommerce.API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult GetProductsAsync()
+        {
+            return Ok();
+        }
+    }
+}
