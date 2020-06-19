bbb145923e5f2be032e968095ba36a4dc7dbe5d8
thaonnp83 <jupiter.planet@gmail.com>
2020-06-19 14:25:40 +0700

    Add api creating new

--- ECommerce.API/Controllers/ProductController.cs
+++ ECommerce.API/Controllers/ProductController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.API.Contexts;
+using ECommerce.API.Domain;
+using ECommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,11 +15,34 @@ namespace ECommerce.API.Controllers
     [ApiController]
     public class ProductController : Controller
     {
-        // GET: /<controller>/
+        private readonly ECommerceContext _dbContext;
+
+        public ProductController(ECommerceContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult GetProductsAsync()
         {
             return Ok();
+        }
+
+        public async Task<IActionResult> AddProductAsync(ProductViewModel productViewModel)
+        {
+            var newProduct = new Product
+                {
+                Name = productViewModel.Name,
+                Description = productViewModel.Description,
+                CategoryId = productViewModel.CategoryId,
+                BrandId = productViewModel.BrandId,
+                Price = productViewModel.Price,
+                ThumbnailUrl = productViewModel.ThumbnailUrl
+                };
+
+            await _dbContext.Products.AddAsync(newProduct);
+           
+            return CreatedAtAction("create", new { id = newProduct.Id });
         }
     }
 }
--- ECommerce.ViewModels/ProductViewModel.cs
+++ ECommerce.ViewModels/ProductViewModel.cs
@@ -4,5 +4,13 @@ namespace ECommerce.ViewModels
 {
     public class ProductViewModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
     }
+    
 }
