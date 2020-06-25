788e720951ae9b6a3ea67797b9998200a96aaac7
thaonnp83 <jupiter.planet@gmail.com>
2020-06-25 17:21:12 +0700

    update brand.cs

--- ECommerce.API/Domain/Brand.cs
+++ ECommerce.API/Domain/Brand.cs
@@ -5,7 +5,9 @@ namespace ECommerce.API.Domain
     public class Brand
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public virtual ICollection<Product> Products { get; set; }
     }
 }
